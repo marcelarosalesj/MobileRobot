7da8cd9f26d2ae5b18ed6410e40c32c18fbf09dd
Marcela Rosales <marcelarosalesj@gmail.com>
2017-04-10 18:29:49 +0000

    dcMotor - Adding motion to the second wheel
    Signed-off-by: Marcela Rosales <marcelarosalesj@gmail.com>

--- dcMotor/dcMotor.py
+++ dcMotor/dcMotor.py
@@ -1,9 +1,10 @@
 import gpiozero
 from time import sleep
 
+print "FIRST WHEEL"
 print "Declaring DC motor"
 # Declaring DC motor over GPIO 27 (forward) and GPIO 22 (backward)
-motor = gpiozero.Motor(27, 22)
+motor = gpiozero.Motor(22, 27)
 
 print "Let's move forward"
 # Forward 2 seconds max speed
@@ -23,3 +24,25 @@ print "Stop"
 
 print "finish"
 
+print "\n\n\nSECOND WHEEL"
+print "Declaring DC motor"
+# Declaring DC motor over GPIO 27 (forward) and GPIO 22 (backward)
+motor2 = gpiozero.Motor(23, 24)
+
+print "Let's move forward"
+# Forward 2 seconds max speed
+motor2.forward(0.5)
+sleep(2)
+motor2.stop()
+
+print "Stop"
+
+print "Let's move backward"
+# Backward 2 seconds max speed
+motor2.backward(0.5)
+sleep(2)
+motor2.stop()
+
+print "Stop"
+
+print "finish"
