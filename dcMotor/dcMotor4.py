3a63d62869b9f258917a0e18859282eaefa8eeaf
= <=>
2018-01-11 05:29:44 +0000

    fix in pin order when creating front left motor, and translate comments

--- dcMotor/dcMotor4.py
+++ dcMotor/dcMotor4.py
@@ -3,13 +3,13 @@ from time import sleep
 
 print "creating motor objects"
 
-# atras derecho e izquierdo
-ad = gpiozero.Motor(22, 27)
-ai = gpiozero.Motor(23, 24)
+# back motors
+br = gpiozero.Motor(22, 27) # back right
+bl = gpiozero.Motor(23, 24) # back left
 
-# frente derecho e izquierdo
-fd = gpiozero.Motor(12,16)
-fi = gpiozero.Motor(5,6)
+# front motors
+fr = gpiozero.Motor(12,16) # front right
+fl = gpiozero.Motor(6,5) # front left
 
 
 print "finish"
