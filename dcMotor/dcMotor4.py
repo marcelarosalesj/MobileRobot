40da97408f356254eaba3a613ddd724ee5f1639e
= <=>
2018-01-13 22:00:48 +0000

    change in dcMotor4

--- dcMotor/dcMotor4.py
+++ dcMotor/dcMotor4.py
@@ -7,9 +7,13 @@ print "creating motor objects"
 br = gpiozero.Motor(22, 27) # back right
 bl = gpiozero.Motor(23, 24) # back left
 
+print "created back motor objects"
+
 # front motors
 fr = gpiozero.Motor(12,16) # front right
 fl = gpiozero.Motor(6,5) # front left
+
+print "created front motor objects"
 
 
 print "finish"
